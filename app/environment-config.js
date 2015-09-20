d32d4518fefd41c7cded138cd0234537b51b619d
stackia <jsq2627@gmail.com>
2015-09-20 16:57:08 +0800

    Revert "Deploy."
    This reverts commit 919998c09975a7d02333c69c281fdb310a4785c2.

--- app/environment-config.js
+++ app/environment-config.js
@@ -2,18 +2,13 @@
     "use strict";
 
     keylolApp.config([
-        "apiEndpointProvider", "httpBatchConfigProvider", "$sceDelegateProvider",
-        function (apiEndpointProvider, httpBatchConfigProvider, $sceDelegateProvider) {
+        "apiEndpointProvider", "httpBatchConfigProvider",
+        function(apiEndpointProvider, httpBatchConfigProvider){
             apiEndpointProvider.setEndPoint("https://testflight-api.keylol.com/");
 
             httpBatchConfigProvider.setAllowedBatchEndpoint("https://testflight-api.keylol.com/", "https://testflight-api.keylol.com/batch", {
                 batchRequestCollectionDelay: 10
             });
-
-            $sceDelegateProvider.resourceUrlWhitelist([
-                "self",
-                "https://keylol-static.b0.upaiyun.com/**"
-            ]);
         }
     ]);
 })();
