3263a07e651676efdc448e9b169e752e129b8ad3
stackia <jsq2627@gmail.com>
2015-09-25 00:12:09 +0800

    提供默认头像。

--- app/components/filters/avatar-url.js
+++ app/components/filters/avatar-url.js
@@ -4,23 +4,7 @@
     keylolApp.filter("avatarUrl", function () {
         return function (input, size) {
             var sizeSuffix;
-            if (input.indexOf("steam://avatars/") === 0) {
-                var hash = input.substring(16);
-                switch (size) {
-                    case "big":
-                        sizeSuffix = "big";
-                        break;
-
-                    case "small":
-                        sizeSuffix = "small";
-                        break;
-
-                    default:
-                        sizeSuffix = "medium";
-                        break;
-                }
-                return "//keylol-steam-cdn.b0.upaiyun.com/steamcommunity/public/images/avatars/" + hash.substring(0, 2) + "/" + hash + "_full.jpg!" + sizeSuffix;
-            } else if (input.indexOf("keylol://avatars/") === 0) {
+            if (input && input.indexOf("keylol://avatars/") === 0) {
                 var path = input.substring(17);
                 switch (size) {
                     case "big":
@@ -37,7 +21,27 @@
                 }
                 return "//keylol.b0.upaiyun.com/" + path + "!" + sizeSuffix;
             }
-            return input;
+
+            var hash;
+            if (input && input.indexOf("steam://avatars/") === 0) {
+                hash = input.substring(16);
+            } else {
+                hash = "fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb";
+            }
+            switch (size) {
+                case "big":
+                    sizeSuffix = "big";
+                    break;
+
+                case "small":
+                    sizeSuffix = "small";
+                    break;
+
+                default:
+                    sizeSuffix = "medium";
+                    break;
+            }
+            return "//keylol-steam-cdn.b0.upaiyun.com/steamcommunity/public/images/avatars/" + hash.substring(0, 2) + "/" + hash + "_full.jpg!" + sizeSuffix;
         };
     });
 })();
