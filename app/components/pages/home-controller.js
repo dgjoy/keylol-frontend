cbbe3592b54b8687ca7d3520dae3718d1d5dd116
戴政 <843976213@qq.com>
2015-09-29 08:27:15 +0800

    修复首页讯息轨道图片不显示。

--- app/components/pages/home-controller.js
+++ app/components/pages/home-controller.js
@@ -36,7 +36,7 @@
                         title: "代工就是不如原厂：这硬仗真的是打的艰辛",
                         summary: "说起这个警匪死磕的话，相比正玩家们没有不知道那大名鼎鼎的CF啊不对CS，也就是《反恐精英（Counter-Strike）》，这款游戏的出现让警匪对殴的模式风靡全球，至今依然有大批玩家热衷于此，除此之外类似的游戏也有不少，例如《彩虹六号系列》，例如《收获日系列》等等，如今FPS界极其有影响力的战地系列也开始涉及此类题材，眼下BETA版本已经开始测试，本篇评测是根据测试版而来，所以并不代表游戏的最终品质！",
                         url: "test",
-                        thumbnail: "//keylol.b0.upaiyun.com/6d1d1b47c1a2500cb7464a83d8600591a37d0f52.jpg!timeline.thumbnail",
+                        thumbnail: "//keylol.b0.upaiyun.com/ffc1c0643b2857caa83ec2cf254e510f.jpg!timeline.thumbnail",
                         count: {
                             like: 666,
                             comment: 222
@@ -79,7 +79,7 @@
                         title: "代工就是不如原厂：这硬仗真的是打的艰辛",
                         summary: "说起这个警匪死磕的话，相比",
                         url: "test",
-                        thumbnail: "//keylol.b0.upaiyun.com/904533661bedd0e06395db660d00417826088ca9.jpg!timeline.thumbnail",
+                        thumbnail: "//keylol.b0.upaiyun.com/da6d364b68257cb921c071d4cea66576.jpg!timeline.thumbnail",
                         count: {
                             like: 666,
                             comment: 222
