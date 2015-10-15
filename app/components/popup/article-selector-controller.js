28047e26c05d8a96bd5dd8b588f5d4cc3065e7b1
戴政 <843976213@qq.com>
2015-10-15 16:10:03 +0800

    完成 article selector popup （除了请求服务器的部分）。 修正据点卡片某些地方的样式实现。

---
+++ app/components/popup/article-selector-controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("ArticleSelectorController", [
+        "$scope", "close", "selectedType",
+        function ($scope, close, selectedType) {
+            $scope.typeOptions = [];
+            for(var i = 0;i < 5;i++){
+                $scope.typeOptions[i] = false;
+            }
+            $scope.typeOptions[selectedType] = true;
+            $scope.changeSelector = function(index){
+                $scope.typeOptions[index] = true;
+                $scope.typeOptions[selectedType] = false;
+                close(index);
+            };
+        }
+    ]);
+})();
