ca9337bef5df9644586a1d59dc66230ec7a52475
戴政 <843976213@qq.com>
2015-10-15 16:59:12 +0800

    完成据点卡片加载动画。

--- app/components/popup/point-preview-card-controller.js
+++ app/components/popup/point-preview-card-controller.js
@@ -2,9 +2,29 @@
     "use strict";
 
     keylolApp.controller("PointPreviewCardController", [
-        "$scope", "data",
-        function ($scope, data) {
-            $scope.data = data;
+        "$scope", "data", "$timeout",
+        function ($scope, data, $timeout) {
+            $scope.loading = true;
+            $timeout(function(){
+                $scope.data = {
+                    action: {
+                        text: "已订阅",
+                        extraClass: "subscribed"
+                    },
+                    head: {
+                        mainHead: "Counter-Strike: Global Offensive",
+                        subHead: "反恐精英：全球攻势"
+                    },
+                    avatar: "assets/images/exit.svg",
+                    background: "//keylol.b0.upaiyun.com/5a3e206aab9decee842a3ea88ac2a312.jpg!point.card.background",
+                    pointSum: {
+                        type: "游戏",
+                        readerNum: 157,
+                        articleNum: 48
+                    }
+                };
+                $scope.loading = false;
+            },data*1000);
         }
     ]);
 })();
--- app/components/sections/reception-controller.js
+++ app/components/sections/reception-controller.js
@@ -48,23 +48,7 @@
                     attachSide: "bottom",
                     align: "left",
                     inputs: {
-                        data: {
-                            action: {
-                                text: "已订阅",
-                                extraClass: "subscribed"
-                            },
-                            head: {
-                                mainHead: "Counter-Strike: Global Offensive",
-                                subHead: "反恐精英：全球攻势"
-                            },
-                            avatar: "assets/images/exit.svg",
-                            background: "//keylol.b0.upaiyun.com/5a3e206aab9decee842a3ea88ac2a312.jpg!point.card.background",
-                            pointSum: {
-                                type: "游戏",
-                                readerNum: 157,
-                                articleNum: 48
-                            }
-                        }
+                        data: 2
                     }
                 });
             };
