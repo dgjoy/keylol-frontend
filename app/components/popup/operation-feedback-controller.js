7a0b7caaa1ab915e85e690cd6c5030057882e964
戴政 <843976213@qq.com>
2015-10-15 20:58:48 +0800

    添加了操作反馈的延时关闭和点击按钮关闭。

--- app/components/popup/operation-feedback-controller.js
+++ app/components/popup/operation-feedback-controller.js
@@ -2,9 +2,17 @@
     "use strict";
 
     keylolApp.controller("OperationFeedbackController", [
-        "$scope", "close", "data",
-        function ($scope, close, data) {
+        "$scope", "close", "data", "$timeout",
+        function ($scope, close, data, $timeout) {
             $scope.data = data;
+            if($scope.data.type != "attention"){
+                $timeout(function(){
+                    close();
+                }, 3000);
+            }
+            $scope.close = function(){
+                close();
+            }
         }
     ]);
 })();
