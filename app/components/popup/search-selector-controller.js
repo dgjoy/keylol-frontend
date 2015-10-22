f8c929c6fe58808b7ea78eb78f629a54d7488c50
戴政 <843976213@qq.com>
2015-10-22 20:45:21 +0800

    新增搜索选择器。

---
+++ app/components/popup/search-selector-controller.js
@@ -0,0 +1,96 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("SearchSelectorController", [
+        "$scope", "union", "$location", "onSearching", "$timeout",
+        function ($scope, union, $location, onSearching, $timeout) {
+            $scope.onSearching = onSearching;
+            $scope.resultArray = [
+                {
+                    avatar: "assets/images/exit.svg",
+                    isUser: true,
+                    mainTitle: "去月球去月球去月球去月球去月球去月球去月球去月球去月球",
+                    subTitle: "To the Moon",
+                    url: "test"
+                },
+                {
+                    avatar: "assets/images/exit.svg",
+                    type: "游戏",
+                    mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                    subTitle: "刀塔",
+                    url: "test"
+                },
+                {
+                    avatar: "assets/images/exit.svg",
+                    type: "游戏",
+                    mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                    subTitle: "刀塔",
+                    url: "test"
+                },
+                {
+                    avatar: "assets/images/exit.svg",
+                    type: "游戏",
+                    mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                    subTitle: "刀塔",
+                    url: "test"
+                },
+                {
+                    avatar: "assets/images/exit.svg",
+                    type: "游戏",
+                    mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                    subTitle: "刀塔",
+                    url: "test"
+                }
+            ];
+            $scope.filterArray = union.searchFilter;
+            $scope.changeFilter = function($index){
+                if(!$scope.filterArray[$index].active){
+                    for(var i in $scope.filterArray){
+                        $scope.filterArray[i].active = false;
+                    }
+                    $scope.filterArray[$index].active = true;
+                }
+                    $scope.resultArray = [
+                        {
+                            avatar: "assets/images/exit.svg",
+                            isUser: true,
+                            mainTitle: "去月球去月球去月球去月球去月球去月球去月球去月球去月球",
+                            subTitle: "To the Moon",
+                            url: "test"
+                        },
+                        {
+                            avatar: "assets/images/exit.svg",
+                            type: "游戏",
+                            mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                            subTitle: "刀塔",
+                            url: "test"
+                        },
+                        {
+                            avatar: "assets/images/exit.svg",
+                            type: "游戏",
+                            mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                            subTitle: "刀塔",
+                            url: "test"
+                        },
+                        {
+                            avatar: "assets/images/exit.svg",
+                            type: "游戏",
+                            mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                            subTitle: "刀塔",
+                            url: "test"
+                        },
+                        {
+                            avatar: "assets/images/exit.svg",
+                            type: "游戏",
+                            mainTitle: "Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2Dota2",
+                            subTitle: "刀塔",
+                            url: "test"
+                        }
+                    ];
+            };
+            $scope.jumpTo = function(url){
+                $location.url(url);
+            }
+        }
+    ]);
+})();
---
+++ app/components/sections/search-box-controller.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("SearchBoxController", [
+        "$scope", "union",
+        function ($scope, union) {
+            $scope.onSearching = function(){
+                console.log(union.searchFilter, $scope.searchText);
+            };
+            $scope.getSearchResults = function($event){
+                $scope.nowPopup = $scope.showSearchSelector({
+                    templateUrl: "components/popup/search-selector.html",
+                    controller: "SearchSelectorController",
+                    attachSide: "bottom",
+                    event: $event,
+                    align: "left",
+                    offsetX: 0,
+                    inputs: {
+                        onSearching: $scope.onSearching
+                    }
+                });
+            };
+            union.searchFilter = [
+                {
+                    text: "据点",
+                    active: true
+                },
+                {
+                    text: "文章"
+                },
+                {
+                    text: "用户"
+                }
+            ];
+        }
+    ]);
+})();
