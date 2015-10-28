aa39f72d17d315b911b6229f524e2a05c5e2e1e4
stackia <jsq2627@gmail.com>
2015-10-28 13:22:38 +0800

    部署最新版。

--- app/root-controller.js
+++ app/root-controller.js
@@ -1,27 +1,23 @@
-﻿(function() {
+﻿(function () {
     "use strict";
 
     keylolApp.controller("RootController", [
         "$scope", "pageTitle", "union", "$http", "apiEndpoint",
-        function($scope, pageTitle, union, $http, apiEndpoint) {
+        function ($scope, pageTitle, union, $http, apiEndpoint) {
             pageTitle.loading();
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return union.$localStorage.login;
-            }, function() {
+            }, function () {
                 var login = union.$localStorage.login;
                 if (login) {
-                    if (login.fromRegistration) {
-                        delete login.fromRegistration;
-                    } else {
-                        $http.get(apiEndpoint + "user/" + login.UserId + "?includeClaims=true")
-                            .then(function(response) {
-                                union.$localStorage.user = response.data;
-                            }, function() {
-                                $http.delete(apiEndpoint + "login/current");
-                                delete union.$localStorage.login;
-                                alert("登录失效，请重新登录。");
-                            });
-                    }
+                    $http.get(apiEndpoint + "user/" + login.UserId, {params: {includeClaims: true}})
+                        .then(function (response) {
+                            union.$localStorage.user = response.data;
+                        }, function () {
+                            $http.delete(apiEndpoint + "login/current");
+                            delete union.$localStorage.login;
+                            alert("登录失效，请重新登录。");
+                        });
                 } else {
                     delete union.$localStorage.user;
                 }
