cf77ecae9224abf771b2a8343f0e4eba198e8585
戴政 <843976213@qq.com>
2015-10-30 16:36:57 +0800

    静态内测首页基本完成。 部署新版本以测试。

--- app/components/pages/alpha-entrance-controller.js
+++ app/components/pages/alpha-entrance-controller.js
@@ -2,19 +2,53 @@
     "use strict";
 
     keylolApp.controller("AlphaEntranceController", [
-        "pageTitle", "$scope",
-        function (pageTitle, $scope) {
+        "pageTitle", "$scope", "$timeout",
+        function (pageTitle, $scope, $timeout) {
             pageTitle.set("内测 - 其乐");
             $scope.secondAnimate = false;
+            var activeLock = 0;
+            $scope.active = 0;
+            var activeTimeout;
             $(window).scroll(function(){
-                if($scope.secondAnimate == false){
-                    $scope.$apply(function(){
+                $scope.$apply(function(){
+                    if($scope.secondAnimate == false){
                         if($(window).scrollTop() >= 768){
                             $scope.secondAnimate = true;
                         }
-                    });
-                }
+                    }
+                    if($(window).scrollTop() < 1300){
+                        $scope.active = 0;
+                    }else if($(window).scrollTop() > 2200){
+                        $scope.active = 6;
+                    }else{
+                        if($(window).scrollTop() < 1480){
+                            changeActive(1);
+                        }else if($(window).scrollTop() < 1660){
+                            changeActive(2);
+                        }else if($(window).scrollTop() < 1840){
+                            changeActive(3);
+                        }else if($(window).scrollTop() < 2020){
+                            changeActive(4);
+                        }else{
+                            changeActive(5);
+                        }
+                    }
+
+                });
             });
+
+            var changeActive = function(index){
+                if(activeLock != index){
+                    activeLock = index;
+                    if(activeTimeout){
+                        $timeout.cancel(activeTimeout);
+                    }
+                    activeTimeout = $timeout(function(){
+                        console.log($(window).scrollTop());
+                        $scope.active = index;
+                    }, 100);
+                }
+            }
         }
     ]);
 })();
