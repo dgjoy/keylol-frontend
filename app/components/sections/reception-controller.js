c7700a03720ce719631435c3ced9bcb8655506a0
stackia <jsq2627@gmail.com>
2015-11-06 01:17:54 +0800

    实现收藏夹 local storage 缓存。

--- app/components/sections/reception-controller.js
+++ app/components/sections/reception-controller.js
@@ -7,79 +7,91 @@
             $scope.quickLinks = [];
             $scope.recentLinks = [];
             $.extend($scope.recentLinks, union.$localStorage.recentBrowse);
-            $http.get(apiEndpoint + "favorite")
-                .then(function(response){
-                    $scope.quickLinks = response.data;
-                    $scope.canBeAdd = $scope.quickLinks.length < 5;
-                    for(var i in $scope.quickLinks){
-                        switch ($scope.quickLinks[i].Type){
-                            case "Unknown":
-                                break;
-                            case "NormalPoint":
-                                if($routeParams.pointIdCode === $scope.quickLinks[i].IdCode){
-                                    $scope.canBeAdd = false;
-                                }
-                                break;
-                            case "ProfilePoint":
-                                if($routeParams.userIdCode === $scope.quickLinks[i].IdCode){
-                                    $scope.canBeAdd = false;
-                                }
-                                break;
-                        }
+
+            var setupQuickLinks = function (quickLinks) {
+                $.extend($scope.quickLinks, quickLinks);
+                $scope.canBeAdd = $scope.quickLinks.length < 5;
+                for (var i in $scope.quickLinks) {
+                    switch ($scope.quickLinks[i].Type) {
+                        case "Unknown":
+                            break;
+                        case "NormalPoint":
+                            if ($routeParams.pointIdCode === $scope.quickLinks[i].IdCode) {
+                                $scope.canBeAdd = false;
+                            }
+                            break;
+                        case "ProfilePoint":
+                            if ($routeParams.userIdCode === $scope.quickLinks[i].IdCode) {
+                                $scope.canBeAdd = false;
+                            }
+                            break;
                     }
-                },function(error){
-                    notification.error("未知错误");
+                }
+            };
+
+            if (union.$localStorage.favorites)
+                setupQuickLinks(union.$localStorage.favorites);
+
+            $http.get(apiEndpoint + "favorite")
+                .then(function (response) {
+                    union.$localStorage.favorites = response.data;
+                    setupQuickLinks(response.data);
+                }, function (response) {
+                    notification.error("未知错误", response);
                 });
-            $scope.deleteFavorite = function(index){
+
+            $scope.deleteFavorite = function (index) {
                 var deleteLink = $scope.quickLinks[index];
                 $http.delete(apiEndpoint + "favorite/" + deleteLink.Id)
-                    .then(function(){
-                        if(deleteLink.Type !== "Unknown" && ($routeParams.pointIdCode === deleteLink.IdCode || $routeParams.userIdCode === deleteLink.IdCode)){
+                    .then(function () {
+                        if (deleteLink.Type !== "Unknown" && ($routeParams.pointIdCode === deleteLink.IdCode || $routeParams.userIdCode === deleteLink.IdCode)) {
                             $scope.canBeAdd = true;
                         }
-                    },function(){
+                    }, function () {
                         notification.error("未知错误");
                     });
                 $scope.quickLinks.splice(index, 1);
             };
-            $scope.addFavorite = function(){
-                if($routeParams.pointIdCode && union.point.Id){
+
+            $scope.addFavorite = function () {
+                if ($routeParams.pointIdCode && union.point.Id) {
                     $scope.canBeAdd = false;
                     $http.post(apiEndpoint + "favorite", {}, {
                         params: {
                             pointId: union.point.Id
                         }
-                    }).then(function(response){
+                    }).then(function (response) {
                         $scope.quickLinks.push({
                             Id: response.data,
                             Type: "NormalPoint",
                             IdCode: union.point.IdCode,
                             Name: union.point.mainName
                         })
-                    },function(){
+                    }, function () {
                         notification.error("未知错误");
                         $scope.canBeAdd = true;
                     });
-                }else if($routeParams.userIdCode && union.user.Id){
+                } else if ($routeParams.userIdCode && union.user.Id) {
                     $scope.canBeAdd = false;
                     $http.post(apiEndpoint + "favorite", {}, {
                         params: {
                             pointId: union.user.Id
                         }
-                    }).then(function(response){
+                    }).then(function (response) {
                         $scope.quickLinks.push({
                             Id: response.data,
                             Type: "ProfilePoint",
                             IdCode: union.user.IdCode,
                             Name: union.user.UserName
                         })
-                    },function(){
+                    }, function () {
                         notification.error("未知错误");
                         $scope.canBeAdd = true;
                     });
                 }
             };
-            $scope.deleteRecentBroswe = function(){
+
+            $scope.deleteRecentBroswe = function () {
                 union.$localStorage.recentBrowse = [];
                 $scope.recentLinks = [];
             }
