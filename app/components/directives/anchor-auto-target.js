d0bac61565dbea74fff17235afe23e2ae0870409
stackia <jsq2627@gmail.com>
2015-11-13 18:21:23 +0800

    a 元素的自动 target 逻辑仅限于 http 或 https 链接。

--- app/components/directives/anchor-auto-target.js
+++ app/components/directives/anchor-auto-target.js
@@ -7,7 +7,8 @@
             link: function (scope, element, attrs) {
                 attrs.$observe("href", function () {
                     var a = element[0];
-                    if (location.host.indexOf(a.hostname) !== 0)
+                    if ((a.protocol.indexOf("http:") === 0 || a.protocol.indexOf("https:") === 0)
+                        && location.host.indexOf(a.hostname) !== 0)
                         a.target = "_blank";
                 });
             }
