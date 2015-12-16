10be76117ab8f37d0d6fded966e3b0534c358a93
戴政 <rexdaniel35@gmail.com>
2015-12-16 20:32:16 +0800

    新增据点信息模块（未完成）。

---
+++ app/components/sections/point-info.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("PointInfoController", [
+        "$scope", "union", "window",
+        function ($scope, union, window) {
+
+        }
+    ]);
+})();
