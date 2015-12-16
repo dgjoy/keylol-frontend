2ada104d90d60e59994eff7afdb51a393ce94a4a
戴政 <rexdaniel35@gmail.com>
2015-12-16 19:35:26 +0800

    简评编辑器的样式实现。 在神秘测试中添加简评编辑器的链接。

---
+++ app/components/directives/point-review.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.directive("pointReview", [
+        function () {
+            return {
+                restrict: "E",
+                templateUrl: "components/directives/point-review.html",
+                scope: {
+                    length: "="
+                },
+                require: "ngModel",
+                link: function (scope, element, attrs, ngModel) {
+                    scope.levelString = ["terrible", "bad", "not-bad", "good", "awesome"];
+                    scope.reviewCircles = [{},{},{},{},{}];
+                    scope.reviewLevel = -1;
+                    scope.reviewString = "";
+                    scope.changeReview = function(i){
+                        for(var k in scope.reviewCircles){
+                            scope.reviewCircles[k].type = "";
+                        }
+                        for(var j = 0;j <= i;j++){
+                            scope.reviewCircles[j].type = scope.levelString[i];
+                        }
+                    };
+                    scope.returnDefault = function(){
+                        scope.changeReview(scope.reviewLevel);
+                    };
+                    scope.setReview = function(i){
+                        scope.reviewLevel = i;
+                        scope.reviewString = scope.levelString[i];
+                    };
+                }
+            };
+        }
+    ]);
+})();
