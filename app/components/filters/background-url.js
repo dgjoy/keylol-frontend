70d38fc9c07a7b3dcb8dcffcee05642534e27712
戴政 <rexdaniel35@gmail.com>
2016-02-29 22:51:01 +0800

    修改默认背景图片。

--- app/components/filters/background-url.js
+++ app/components/filters/background-url.js
@@ -5,7 +5,7 @@
         "$filter",
         function ($filter) {
             return function (input) {
-                return $filter("uriRelocate")(input, "profile.point.background", "keylol://991a9466477b8f5b49dbd034fc93bcd5.jpg");
+                return $filter("uriRelocate")(input, "profile.point.background", "keylol://0f9b6041e0354a7ae741e2650fa0c066.jpg");
             };
         }
     ]);
