3c31fb12195397ff2ff7616d66ff63f0bfdaeae2
stackia <jsq2627@gmail.com>
2016-03-05 16:55:38 +0800

    调整 keylol-frontend.sh。

--- keylol-frontend.sh
+++ keylol-frontend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -z "$API_ENDPOINT" ]; then
+  echo "Use default API endpoint: https://api.keylol.com/"
+else
   OLD=$(echo 'https://api.keylol.com/' | sed -e 's/[]\/$*.^|[]/\\&/g')
   NEW=$(echo $API_ENDPOINT | sed -e 's/[]\/$*.^|[]/\\&/g')
   sed -i 's/'"$OLD"'/'"$NEW"'/g' /usr/share/nginx/html/bundles/app-*.js
-  echo "Using API endpoint $API_ENDPOINT ..."
-else
-  echo "Using default API endpoint ..."
+  echo "Use API endpoint: $API_ENDPOINT"
 fi
-nginx -g daemon off
+nginx -g 'daemon off;'
