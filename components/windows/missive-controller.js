7c6e3f7ac6a0a65592e9b6e8909bdd3c63ebc23c
ZenDay <843976213@qq.com>
2016-03-18 20:39:34 +0800

    实现了公函的外部样式。

---
+++ components/windows/missive-controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("MissiveController", [
+        "$scope", "close", "$timeout", "$http", "notification",
+        function ($scope, close, $timeout, $http, notification) {
+            $scope.cancel = function () {
+                close();
+            };
+        }
+    ]);
+})();
