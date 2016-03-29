0a3788ff183a01980ecac7c2156269dc47c6328a
ZenDay <843976213@qq.com>
2016-03-29 19:28:51 +0800

    完成了返回顶部按钮。

---
+++ components/sections/site-footer-controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    keylolApp.controller("SiteFooterController", [
+        "$scope", "$element", "$window",
+        function ($scope, $element, $window) {
+            $scope.scrollToTop = function () {
+                $("body").animate({
+                    scrollTop: 0
+                });
+            };
+
+            var $$window = $($window);
+            $$window.scroll(function () {
+                if(!$scope.canScrollTop && $$window.scrollTop() + $$window.height() > $element.offset().top + $element.height() + 100){
+                    $scope.canScrollTop = true;
+                }else if($scope.canScrollTop && $$window.scrollTop() + $$window.height() <= $element.offset().top + $element.height() + 100){
+                    $scope.canScrollTop = false;
+                }
+            });
+            var cancelListenRoute = $scope.$on("$destroy", function () {
+                $$window.unbind("scroll");
+                cancelListenRoute();
+            });
+        }
+    ]);
+})();
