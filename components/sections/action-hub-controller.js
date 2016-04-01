ba3358855f46c737871ddee2a50087a0a60a5c99
ZenDay <843976213@qq.com>
2016-04-01 13:23:59 +0800

    完成了文券变更时的动画样式。

--- components/sections/action-hub-controller.js
+++ components/sections/action-hub-controller.js
@@ -2,8 +2,8 @@
     "use strict";
 
     keylolApp.controller("ActionHubController", [
-        "$scope", "union", "window",
-        function ($scope, union, window) {
+        "$scope", "union", "window", "$timeout",
+        function ($scope, union, window, $timeout) {
             $scope.union = union;
             if(union.$localStorage.user){
                 $scope.$watch('union.$localStorage.user.MessageCount', function (newValue) {
@@ -12,6 +12,21 @@
                     }):[0,0,0];
                 });
             }
+
+            $scope.couponChanges = [];
+
+            $scope.fakeCoupon = 26;
+            $timeout(function () {
+                $scope.couponChanges.push({
+                    beforeChange: 26,
+                    Event: "发布文章",
+                    changeCount: -3
+                });
+                $scope.fakeCoupon = null;
+                $timeout(function () {
+                    $scope.hideAnimate = true;
+                }, 2000)
+            },1000);
 
             $scope.showRegistrationWindow = function () {
                 window.show({
