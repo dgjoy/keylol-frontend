343f78b066869b49f8153a0c82da63590abc5bde
ZenDay <843976213@qq.com>
2016-04-21 21:16:37 +0800

    再次尝试更改复制命令。

--- keylol-frontend.sh
+++ keylol-frontend.sh
@@ -20,7 +20,7 @@ case ${ACTION} in
       prod)
         cp -rf assets ${BUILD_COPY_TARGET}/
         cp -rf bundles ${BUILD_COPY_TARGET}/
-        cp -rf node_modules/simditor/styles/fonts ${BUILD_COPY_TARGET}/
+        cp -rf node_modules/simditor/styles/fonts ${BUILD_COPY_TARGET}/node_modules/simditor/styles/
         cp -f index.html ${BUILD_COPY_TARGET}/
         cp -f user_agreement.pdf ${BUILD_COPY_TARGET}/
         rm -rf ${BUILD_COPY_TARGET}/assets/stylesheets
