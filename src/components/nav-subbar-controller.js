2e889e59c70513669956a927f0c757e56027922e
Guihong Ge <2541615457@qq.com>
2016-05-03 21:45:44 +0800

    完成 navSubbar 组件的界面。

---
+++ src/components/nav-subbar-controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    class NavSubbarController {
+        constructor ($scope, union, $http, apiEndpoint, notification) {
+            
+        }
+    }
+
+    keylolApp.component('navSubbar', {
+        templateUrl: 'src/components/nav-subbar.html',
+        controller: NavSubbarController,
+        controllerAs: 'navSubbar',
+    });
+}());
