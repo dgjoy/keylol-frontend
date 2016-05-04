594e0947ea8becde36dce66c5dd48b8eff66cb40
Guihong Ge <2541615457@qq.com>
2016-05-04 21:20:20 +0800

    实现 tabs 组件。

---
+++ src/components/tabs-controller.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    class tabsController {
+        constructor ($scope, union, $http, apiEndpoint, notification, $timeout) {
+
+            $timeout(() => {
+                const curElement = $('.link').eq(this.curTab);
+                this.pWidth = curElement.parent().width();
+                $('.move-bar').css({
+                    'left': curElement.position().left ,
+                    'right': this.pWidth - curElement.position().left - curElement.width() ,
+                });
+            });
+        }
+
+        moveBar(index) {
+            this.curTab = index;
+
+            const jLinks = $('.link');
+            const jMoveBar = $('.move-bar');
+
+            const curElement = jLinks.eq(index);
+            const oldLeft = jMoveBar.css('left').replace('px','');
+            const oldRight = jMoveBar.css('right').replace('px','');
+            const newLeft = curElement.position().left;
+            const newRight = this.pWidth - curElement.position().left - curElement.width();
+
+            if (oldLeft < newLeft) {
+                jMoveBar.animate({'right': newRight,},{duration:150, queue:false,});
+                jMoveBar.animate({'left': newLeft,},{duration:250, queue:false,});
+            } else if (oldLeft > newLeft) {
+                jMoveBar.animate({'left': newLeft,},{duration:150, queue:false,});
+                jMoveBar.animate({'right': newRight,},{duration:250, queue:false,});
+            }
+        }
+    }
+
+    keylolApp.component('tabs', {
+        templateUrl: 'src/components/tabs.html',
+        controller: tabsController,
+        controllerAs: 'tabs',
+        bindings:{
+            tabArray: '<',
+            curTab: '@',
+        },
+    });
+}());
