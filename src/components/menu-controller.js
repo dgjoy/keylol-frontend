8a9a6700411f4fdfb188ffb5cd85b61a9cc9ca31
ZenDay <843976213@qq.com>
2016-05-05 12:16:12 +0800

    新增 UIC 专用字体。 将 nav 放到discovery中，目前只有discovery可以访问。 改变 container 宽度为 940px（ PC 端 ）。 新增 menu component （还未完成）。

---
+++ src/components/menu-controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    class MenuController {}
+
+    keylolApp.component('menu', {
+        templateUrl: 'src/components/menu.html',
+        controller: MenuController,
+        controllerAs: 'menu',
+        bindings: {
+            text: '@',
+        },
+    });
+}());
