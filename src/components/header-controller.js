f8d4df8fae28ec388bc37e31ec9b13307e78872d
Guihong Ge <2541615457@qq.com>
2016-05-06 17:32:00 +0800

    实现 header 组件。 修复 slide-show 字体颜色问题。

---
+++ src/components/header-controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    class headerController {}
+
+    keylolApp.component('header', {
+        templateUrl: 'src/components/header.html',
+        controller: headerController,
+        controllerAs: 'header',
+        bindings: {
+            isOrange: '<',
+            mainTitle: '@',
+            subTitle: '@',
+        },
+    });
+}());
