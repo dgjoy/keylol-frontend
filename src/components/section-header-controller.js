dcf058fc737f39b2ac99e2756fb5e2578ce9dc0d
Guihong Ge <2541615457@qq.com>
2016-05-07 11:45:53 +0800

    修改 sectionHeader 传入属性的方式。 完善 spotlightArticles 的样式。

--- src/components/section-header-controller.js
+++ src/components/section-header-controller.js
@@ -6,9 +6,7 @@
         controller: SectionHeaderController,
         controllerAs: 'sectionHeader',
         bindings: {
-            isOrange: '<',
-            mainTitle: '<',
-            subTitle: '<',
+            header: '<',
         },
     });
 }());
