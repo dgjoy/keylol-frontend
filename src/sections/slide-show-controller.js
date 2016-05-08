c9d65ddd91b382c869e3347194d472e627bc0e12
Guihong Ge <2541615457@qq.com>
2016-05-08 11:08:13 +0800

    修改幻灯片滑动动画的实现方式。

--- src/sections/slide-show-controller.js
+++ src/sections/slide-show-controller.js
@@ -12,7 +12,7 @@
                 ename: 'Rogue Stormers',
                 author: '@Xxxxxxx',
                 date: '5月6号',
-                img: '//storage.keylol.com/39b7d020df471d6c0e3fa593802000bc.jpg!timeline.thumbnail.new',
+                img: '//storage.keylol.com/37ba4e25767b3b4c1417f4c625e88492.jpg',
                 desc: '《恶棍风暴兵团》 是一款现代版Contra／/Metal Slug 游戏，它结合了 这种roguelike 类型的混搭游戏，并包括和 RPG 角色扮演的元素，游戏中有着、加上疯狂的3D 图形与可支持一至四人的多人游戏合作模式。',
             },{
                 title: 'Rogue Stormer（恶棍风暴兵团） Boss攻略与成就指南',
@@ -28,7 +28,7 @@
                 ename: 'Rogue Stormers',
                 author: '@Xxxxxxx',
                 date: '5月6号',
-                img: '//storage.keylol.com/39b7d020df471d6c0e3fa593802000bc.jpg!timeline.thumbnail.new',
+                img: '',
                 desc: '《恶棍风暴兵团》 是一款现代版Contra／/Metal Slug 游戏，它结合了 这种roguelike 类型的混搭游戏，并包括和 RPG 角色扮演的元素，游戏中有着、加上疯狂的3D 图形与可支持一至四人的多人游戏合作模式。',
             },{
                 title: 'Rogue Stormer（恶棍风暴兵团） Boss攻略与成就指南',
@@ -43,13 +43,18 @@
             this.slides = tmp_slides;
             this.indexCount = this.slides.length;
             this.index = 0;
+            this.oldIndex = -1;
             this.isMoveUp = false;
 
             this.resetTimer();
         }
         
         slideTo(index) {
+            if (index === this.index)
+                return ;
+
             this.isMoveUp = (index < this.index);
+            this.oldIndex = this.index;
             this.index = index;
 
             this.resetTimer();
