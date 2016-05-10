5086bc210f219aff4310218938d11ba490feebfa
Guihong Ge <2541615457@qq.com>
2016-05-10 17:34:08 +0800

    完成用户卡片的部分样式。

---
+++ src/components/user-card-controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    class UserCardController {
+
+    }
+
+    keylolApp.component('userCard', {
+        templateUrl: 'src/components/user-card.html',
+        controller: UserCardController,
+        controllerAs: 'userCard',
+        bindings: {
+            text: '@',
+        },
+    });
+}());
