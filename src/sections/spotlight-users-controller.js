2f5f78ddb0b5b0e8c5993ca5241d886bb54d63f7
Guihong Ge <2541615457@qq.com>
2016-05-11 15:22:18 +0800

    实现用户卡片的界面。 实现 spotlightUsers 的界面。 将一些卡片的 img 标签改为 带背景的 div。

---
+++ src/sections/spotlight-users-controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    class SpotlightUsersController {
+        constructor ($element) {
+            this.steamFriendCardsPart1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            this.steamFriendCardsPart2 = [7, 8, 9];
+            this.sameFriendCards = [1,2];
+            this.type = {
+                mainTitle: '玩家',
+                subTitle: '与游戏好友一起在其乐融融',
+                isOrange: true,
+            };
+        }
+    }
+
+    keylolApp.component('spotlightUsers', {
+        templateUrl: 'src/sections/spotlight-users.html',
+        controller: SpotlightUsersController,
+        controllerAs: 'spotlightUsers',
+        bindings: {
+        },
+    });
+}());
