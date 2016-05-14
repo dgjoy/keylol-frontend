45ef392e4ee6205ea86a6dfb0c9e38cc09c7fbb2
Guihong Ge <2541615457@qq.com>
2016-05-14 17:29:33 +0800

    实现 textArea 组件的样式。

---
+++ src/components/text-area-controller.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    class TextAreaController {
+        constructor (stateTree, $http, apiEndpoint, notification) {
+            $.extend(this, {
+                stateTree,
+                $http,
+                apiEndpoint,
+                notification,
+            });
+
+            this.active = false;
+            this.value = '';
+            if (this.value.length !== 0){
+                this.filled = true;
+            }
+
+            this.defaultTip = '日常界面展示的称呼';
+            this.tip = this.defaultTip;
+            this.localWarn = false;
+            this.locked = false;
+        }
+
+        focus() {
+            this.active = true;
+        }
+
+        blur() {
+            this.active = false;
+            this.filled = (this.value.length !== 0);
+        }
+
+        localCheck() {
+            if (this.value.length > 5) {
+                this.localWarn = true;
+                this.tip = '不得超过5个字符';
+            } else {
+                this.localWarn = false;
+                this.tip = this.defaultTip;
+            }
+        }
+    }
+
+    keylolApp.component('textArea', {
+        templateUrl: 'src/components/text-area.html',
+        controller: TextAreaController,
+        controllerAs: 'textArea',
+    });
+}());
