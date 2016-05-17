d9345296694f3d16cc450fdb1ac68c5d1522b2ea
Guihong Ge <2541615457@qq.com>
2016-05-17 18:29:09 +0800

    实现认同列表的样式。

--- src/components/timeline-card-controller.js
+++ src/components/timeline-card-controller.js
@@ -9,6 +9,21 @@
         open() {
             this.state = 'open';
         }
+        
+        showApproverList($event) {
+            this.showApproverListPopup({
+                templateUrl: 'src/popup/approver-list.html',
+                controller: 'ApproverListController as approverList',
+                event: $event,
+                attachSide: 'right',
+                align: 'bottom',
+                offsetX: 60,
+                offsetY: 120,
+                showDelay: 0,
+                closeDelay: 0,
+                inputs: { content: 'hello' },
+            });
+        }
     }
 
     keylolApp.component('timelineCard', {
--- src/sections/section-timeline-controller.js
+++ src/sections/section-timeline-controller.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     class SectionTimelineController {
-        constructor ($rootScope, $location) {
+        constructor ($scope, $rootScope, $location) {
             $.extend(this, {
                 $rootScope,
                 $location,
@@ -12,12 +12,29 @@
                 this.leftCards = ['l', 'l'];
                 this.rightCards = ['r'];
             }
+
+            this.firstLoad = true;
+
+            const scrollHandler = $(window).on('scroll', () => {
+                if (!this.firstLoad) {
+                    if ($(document).scrollTop() + $(window).height() >= $(document).height()) {
+                        $scope.$apply(() => {
+                            this.load();
+                        });
+                    }
+                }
+            });
+
+            $scope.$on('$destroy',() => {
+               $(window).unbind('scroll',scrollHandler);
+            });
         }
 
         load() {
+            this.firstLoad = false;
             if (this.columnCount === 'two') {
-                this.leftCards.push(['l']);
-                this.rightCards.push(['r', 'r']);
+                this.leftCards.push(1);
+                this.rightCards.push(1);
             } else {
                 this.cards.push('l');
             }
