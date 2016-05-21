5000e98c460127e401a078d9a7036ada4edf738f
ZenDay <843976213@qq.com>
2016-05-21 16:07:32 +0800

    图标 viewbox 变为 64。

--- src/filters/icon-svg-url.js
+++ src/filters/icon-svg-url.js
@@ -1,7 +1,7 @@
 (function () {
     keylolApp.filter('iconSvgUrl', () => {
         return function (input) {
-            return `assets/images/sprite-9e7161ecf5.svg#${input}`;
+            return `assets/images/sprite-1a88a4d9fc.svg#${input}`;
         };
     });
 }());
