9b2365926119668c07437609a84f2919ee88a5bc
Guihong Ge <2541615457@qq.com>
2016-05-29 17:12:10 +0800

    完成文章页的大部分界面。

--- src/filters/icon-svg-url.js
+++ src/filters/icon-svg-url.js
@@ -1,7 +1,7 @@
 (function () {
     keylolApp.filter('iconSvgUrl', () => {
         return function (input) {
-            return `assets/images/sprite-21fa3c1da1.svg#${input}`;
+            return `assets/images/sprite-a0225b3068.svg#${input}`;
         };
     });
 }());
