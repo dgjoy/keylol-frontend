c60efc03d3ad7c6af8b4233aab649fde7225c44c
ZenDay <843976213@qq.com>
2016-05-31 08:13:06 +0800

    修复了情报页打开时的错误。

--- src/pages/intel-controller.js
+++ src/pages/intel-controller.js
@@ -1,19 +1,17 @@
 ﻿(function () {
     class IntelController {
-        constructor ($scope, pageHead, stateTree, $location, pageLoad, $stateParams) {
+        constructor ($scope, pageHead, stateTree, pageLoad, $state) {
             pageHead.setTitle('据点 - 情报 - 其乐');
 
-            if (!$location.url().match(/\/point\/[^\/]*\/?$/)) {
-                if (stateTree.aggregation && stateTree.aggregation.point
-                    && stateTree.aggregation.point.idCode === $state.params.point_id_code ) {
-                    pageLoad('aggregation.point.intel');
-                } else {
-                    pageLoad('aggregation.point', { entrance: 'Intel' }).then(result => {
-                        if (result) {
-                            result.idCode = $state.params.point_id_code;
-                        }
-                    });
-                }
+            if (stateTree.empty || stateTree.aggregation && stateTree.aggregation.point
+                && stateTree.aggregation.point.idCode === $state.params.point_id_code ) {
+                pageLoad('aggregation.point.intel');
+            } else {
+                pageLoad('aggregation.point', { entrance: 'Intel' }).then(result => {
+                    if (result) {
+                        result.idCode = $state.params.point_id_code;
+                    }
+                });
             }
             
             $scope.stateTree = stateTree;
