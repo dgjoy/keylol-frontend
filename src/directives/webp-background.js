653a0e85a23a1fa467473eb899666b76ec2b587b
ZenDay <843976213@qq.com>
2016-06-03 21:00:16 +0800

    修复了 SVG 无法显示的问题。 修复了当 webp-background 为空时，会请求一次主页的问题。

--- src/directives/webp-background.js
+++ src/directives/webp-background.js
@@ -5,10 +5,15 @@
             priority: 98,
             link (scope, element, attrs) {
                 attrs.$observe('webpBackground', value => {
+                    if (!value) {
+                        return;
+                    }
+
                     if (/\.svg|gif$/i.test(value)) {
                         element.css('background-image', `url(${value})`);
                         return;
                     }
+                    
                     if (/^(?:http:|https:)?\/\/(storage|steamcdn)\.keylol\.com\//i.test(value)) {
                         utils.supportWebp.then(() => {
                             if (value.indexOf('!') === -1) {
--- src/filters/icon-svg-url.js
+++ src/filters/icon-svg-url.js
@@ -1,7 +1,7 @@
 (function () {
     keylolApp.filter('iconSvgUrl', () => {
         return function (input) {
-            return `assets/images/sprite-a0225b3068.svg#${input}`;
+            return `assets/images/sprite-14f22a2430.svg#${input}`;
         };
     });
 }());
