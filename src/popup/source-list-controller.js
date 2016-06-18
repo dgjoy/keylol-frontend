ff6b5c852d3d789f5e7ff268f931f47bf59e3c41
Guihong Ge <2541615457@qq.com>
2016-06-18 21:42:32 +0800

    减小按钮的上下 padding。 完善收稿据点的样式。

--- src/popup/source-list-controller.js
+++ src/popup/source-list-controller.js
@@ -1,6 +1,25 @@
 (function () {
     class SourceListController {
-    
+        constructor() {
+            this.mainPoint = {
+                avatarImage: 'keylol://37ba4e25767b3b4c1417f4c625e88492.jpg',
+                chineseName: '刀塔',
+                englishName: 'Dota 2',
+                idCode: 'DOTA2',
+            };
+
+            this.points = [{
+                avatarImage: 'keylol://8b471e9e7181c0c7c58828d31d6b08f5.png',
+                chineseName: '角色扮演',
+                englishName: 'RPG',
+                idCode: 'RPLAY',
+            },{
+                avatarImage: 'keylol://4ed8e4862422e8a584fb6bee9f32fedd.jpg',
+                chineseName: '刺客信条 2',
+                englishName: 'Assassin\'s Creed 2',
+                idCode: 'ASCR2',
+            }];
+        }
     }
     
     keylolApp.controller('SourceListController', SourceListController);
