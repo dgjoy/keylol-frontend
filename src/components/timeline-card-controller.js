074a384dac01b7404de1d36953925e6104c670e2
ZenDay <843976213@qq.com>
2016-06-23 18:15:56 +0800

    完成轨道上的评论相关操作。

--- src/components/timeline-card-controller.js
+++ src/components/timeline-card-controller.js
@@ -1,11 +1,14 @@
 ﻿(function () {
     class TimelineCardController {
-        constructor($element, $timeout, utils, stateTree) {
+        constructor($element, $timeout, utils, stateTree, $http, apiEndpoint, notification) {
             $.extend(this,{
                 $element,
                 $timeout,
                 utils,
                 stateTree,
+                $http,
+                apiEndpoint,
+                notification,
             });
             this.state = 'close';
             this.hasScroller = true;
@@ -41,7 +44,63 @@
         }
 
         openReviewArea() {
-            this.state = 'open';
+            this.$http.get(`${this.apiEndpoint}states/entrance/timeline/cards[${this.card.contentId}]/comments`, {
+                params: {
+                    take: 30,
+                },
+            }).then(response => {
+                this.comments = response.data;
+                console.log(this.comments);
+                this.state = 'open';
+
+                this.$timeout(() => {
+                    const e = this.$element.find('.review-list');
+                    e.animate({
+                        scrollTop: e.find(`[data-floor-id='#${this.comments[this.comments.length - 1].sidForActivity}']`).position().top,
+                    });
+                });
+            }, response => {});
+        }
+
+        replyComment(comment) {
+            if (!this.comment) {
+                this.comment = `#${comment.sidForActivity} `;
+            } else {
+                this.comment += `\n#${comment.sidForActivity} `;
+            }
+            this.commentFocus = true;
+        }
+
+        doComment() {
+            this.submitLock = true;
+            const submitObj = {
+                content: this.comment,
+                activityId: this.card.contentId,
+            };
+
+            this.$http.post(`${this.apiEndpoint}activity-comment`,submitObj).then(response => {
+                this.comments.push({
+                    authorAvatarImage: this.stateTree.currentUser.avatarImage,
+                    authorIdCode: this.stateTree.currentUser.idCode,
+                    authorUserName: this.stateTree.currentUser.userName,
+                    content: this.comment,
+                    likeCount: 0,
+                    publishTime: new Date().toISOString(),
+                    sidForActivity: response.data,
+                });
+                this.comment = '';
+                this.submitLock = false;
+                this.$timeout(() => {
+                    const e = this.$element.find('.review-list');
+                    e.animate({
+                        scrollTop: e.find(`[data-floor-id='#${response.data}']`).position().top,
+                    });
+                });
+                this.notification.success({ message: '提交成功' });
+            }, response => {
+                this.notification.error({ message: '发生未知错误，请重试或与站务职员联系' }, response);
+                this.submitLock = false;
+            });
         }
 
         showSourceList($event) {
