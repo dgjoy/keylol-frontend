278d37a191f78462e965f2ee3cea78e07337400a
ZenDay <843976213@qq.com>
2016-07-20 11:53:31 +0800

    完成邀请好友。

--- src/components/text-area-controller.js
+++ src/components/text-area-controller.js
@@ -63,6 +63,7 @@
             theme:'<',
             whitelist: '<',
             options: '<',
+            selectedOnFocus: '<',
         },
     });
 }());
--- src/windows/friend-invitation-controller.js
+++ src/windows/friend-invitation-controller.js
@@ -1,8 +1,15 @@
 ﻿(function () {
     class FriendInvitationController {
-        constructor(close) {
+        constructor(close, $http, apiEndpoint, notification, stateTree) {
             $.extend(this, {
                 close,
+                inviteLink: `https://www.keylol.com/?aff=${stateTree.currentUser.idCode}`,
+            });
+
+            $http.get(`${apiEndpoint}states/invite-count`).then(response => {
+                this.inviteCount = response.data;
+            }, response => {
+                notification.error({ message: '发生未知错误，请重试或与站务职员联系' }, response);
             });
         }
     }
